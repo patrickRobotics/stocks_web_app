e17bada887574bbc1ba3fa1f260f7f7599cdc48d
patrickRobotics <pirungu9@gmail.com>
2021-10-01 16:25:37 +0300

    add header and footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header";
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
+      <Footer />
     </div>
   );
 }
