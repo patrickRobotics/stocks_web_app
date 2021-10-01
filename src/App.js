3ebaf3b59a82a04f075f79e34a81ad82f585e073
patrickRobotics <pirungu9@gmail.com>
2021-10-01 21:04:24 +0300

    page setup using material ui theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
-import Header from "./components/Header";
+import { createTheme, ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import Header from './components/Header'
 import Footer from './components/Footer';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main:"#2e11667",
+    },
+    secondary: {
+      main: "#c7dBed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+    },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
+
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center"
+  },
+  bigSpace: {
+    marginTop: "5rem"
+  },
+  littleSpace:{
+    marginTop: "2.5rem",
+  },
+  grid:{
+    display: "flex", 
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap", 
+  },
+})
 
 function App() {
+  const classes = styles(); 
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <Header />
+
+        <div className={classes.bigSpace}>
+          <Footer />
+        </div>
+      </ThemeProvider>
+
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,34 @@
+import {Toolbar, Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles"; 
+
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor: "#eeeeef",
+        ['@media (max-width:780px)']: {
+           flexDirection: "column"
+        }
+    },
+    menuItem: {
+        cursor: "pointer",
+        flexGrow: 1,
+        "&:hover": {
+            color:  "#4f25c8"
+        },
+        ['@media (max-width:780px)']: { 
+            paddingBottom: "1rem"
+        }
+    }
+})
+
 const Header = () => {
+    const classes = styles()
     return (
-        <header className='header'>
-            <h1>Company Stocks Market</h1>
-        </header>
+        <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
+            <Typography variant="h6" className={classes.menuItem}>
+                Company Stock Markets
+            </Typography>
+        </Toolbar>
     )
 }
 
