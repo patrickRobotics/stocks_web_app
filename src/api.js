1b8c79cc41632529df732d92158c401692f6c8bc
patrickRobotics <pirungu9@gmail.com>
2021-10-04 01:08:48 +0300

    replace json-server with mock API

---
+++ src/api.js
@@ -0,0 +1,315 @@
+//pseudo db
+const batsCodes = {
+    "codes": [
+        {
+        "code": "BATS_A",
+        "name": "Short Sale Market for Stock Symbol A on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: A<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2008-01-02",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AA",
+        "name": "Short Sale Market for Stock Symbol AA on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AA<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2008-01-02",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAA",
+        "name": "Short Sale Market for Stock Symbol AAA on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAA<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:02:14",
+        "from_date": "2020-09-11",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAAP",
+        "name": "Short Sale Market for Stock Symbol AAAP on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAAP<br><br>Exchange: BATS",
+        "refreshed_at": "2018-02-16 19:03:35",
+        "from_date": "2015-11-11",
+        "to_date": "2018-02-09"
+        },
+        {
+        "code": "BATS_AAAU",
+        "name": "Short Sale Market for Stock Symbol AAAU on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAAU<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2018-08-22",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AABA",
+        "name": "Short Sale Market for Stock Symbol AABA on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AABA<br><br>Exchange: BATS",
+        "refreshed_at": "2019-10-09 19:03:19",
+        "from_date": "2017-06-19",
+        "to_date": "2019-10-02"
+        },
+        {
+        "code": "BATS_AAC",
+        "name": "Short Sale Market for Stock Symbol AAC on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAC<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2008-01-29",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AACC",
+        "name": "Short Sale Market for Stock Symbol AACC on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AACC<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 19:42:50",
+        "from_date": "2008-01-02",
+        "to_date": "2013-06-13"
+        },
+        {
+        "code": "BATS_AACG",
+        "name": "Short Sale Market for Stock Symbol AACG on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AACG<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2019-10-17",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AACIU",
+        "name": "Short Sale Market for Stock Symbol AACIU on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AACIU<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:02:17",
+        "from_date": "2021-08-13",
+        "to_date": "2021-09-24"
+        },
+        {
+        "code": "BATS_AACOU",
+        "name": "Short Sale Market for Stock Symbol AACOU on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AACOU<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 20:40:56",
+        "from_date": "2010-11-24",
+        "to_date": "2012-02-10"
+        },
+        {
+        "code": "BATS_AACOW",
+        "name": "Short Sale Market for Stock Symbol AACOW on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AACOW<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 20:46:21",
+        "from_date": "2011-05-16",
+        "to_date": "2012-07-24"
+        },
+        {
+        "code": "BATS_AACQ",
+        "name": "Short Sale Market for Stock Symbol AACQ on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AACQ<br><br>Exchange: BATS",
+        "refreshed_at": "2021-07-01 19:00:55",
+        "from_date": "2020-09-09",
+        "to_date": "2021-06-24"
+        },
+        {
+        "code": "BATS_AACQU",
+        "name": "Short Sale Market for Stock Symbol AACQU on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AACQU<br><br>Exchange: BATS",
+        "refreshed_at": "2021-06-30 19:01:09",
+        "from_date": "2020-07-14",
+        "to_date": "2021-06-23"
+        },
+        {
+        "code": "BATS_AACQW",
+        "name": "Short Sale Market for Stock Symbol AACQW on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AACQW<br><br>Exchange: BATS",
+        "refreshed_at": "2021-07-01 19:00:55",
+        "from_date": "2020-09-08",
+        "to_date": "2021-06-24"
+        },
+        {
+        "code": "BATS_AAC_U",
+        "name": "Short Sale Market for Stock Symbol AAC.U on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAC.U<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-27 19:00:57",
+        "from_date": "2021-02-02",
+        "to_date": "2021-09-20"
+        },
+        {
+        "code": "BATS_AAC_WS",
+        "name": "Short Sale Market for Stock Symbol AAC.WS on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAC.WS<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2021-03-26",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AADI",
+        "name": "Short Sale Market for Stock Symbol AADI on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AADI<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2021-08-27",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AADR",
+        "name": "Short Sale Market for Stock Symbol AADR on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AADR<br><br>Exchange: BATS",
+        "refreshed_at": "2021-08-19 19:00:55",
+        "from_date": "2010-08-31",
+        "to_date": "2021-08-12"
+        },
+        {
+        "code": "BATS_AAI",
+        "name": "Short Sale Market for Stock Symbol AAI on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAI<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 19:42:50",
+        "from_date": "2008-01-02",
+        "to_date": "2011-05-02"
+        },
+        {
+        "code": "BATS_AAIC",
+        "name": "Short Sale Market for Stock Symbol AAIC on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAIC<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2020-10-26",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAIC_PB",
+        "name": "Short Sale Market for Stock Symbol AAICpB on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAICpB<br><br>Exchange: BATS",
+        "refreshed_at": "2021-08-17 19:00:49",
+        "from_date": "2020-10-27",
+        "to_date": "2021-08-10"
+        },
+        {
+        "code": "BATS_AAIC_PC",
+        "name": "Short Sale Market for Stock Symbol AAICpC on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAICpC<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-07 19:00:55",
+        "from_date": "2020-11-10",
+        "to_date": "2021-08-31"
+        },
+        {
+        "code": "BATS_AAII",
+        "name": "Short Sale Market for Stock Symbol AAII on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAII<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 20:36:25",
+        "from_date": "2008-02-29",
+        "to_date": "2009-04-01"
+        },
+        {
+        "code": "BATS_AAIN",
+        "name": "Short Sale Market for Stock Symbol AAIN on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAIN<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-23 19:01:06",
+        "from_date": "2021-07-22",
+        "to_date": "2021-09-16"
+        },
+        {
+        "code": "BATS_AAIT",
+        "name": "Short Sale Market for Stock Symbol AAIT on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAIT<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 20:47:03",
+        "from_date": "2012-11-23",
+        "to_date": "2015-08-05"
+        },
+        {
+        "code": "BATS_AAL",
+        "name": "Short Sale Market for Stock Symbol AAL on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAL<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2013-12-09",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AALCP",
+        "name": "Short Sale Market for Stock Symbol AALCP on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AALCP<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 20:47:13",
+        "from_date": "2013-12-09",
+        "to_date": "2014-04-07"
+        },
+        {
+        "code": "BATS_AAMC",
+        "name": "Short Sale Market for Stock Symbol AAMC on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAMC<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2013-09-16",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAME",
+        "name": "Short Sale Market for Stock Symbol AAME on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAME<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2008-01-04",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAN",
+        "name": "Short Sale Market for Stock Symbol AAN on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAN<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2009-04-20",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAN_A",
+        "name": "Short Sale Market for Stock Symbol AAN/A on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAN/A<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 20:40:38",
+        "from_date": "2009-05-18",
+        "to_date": "2010-12-07"
+        },
+        {
+        "code": "BATS_AANB",
+        "name": "Short Sale Market for Stock Symbol AANB on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AANB<br><br>Exchange: BATS",
+        "refreshed_at": "2016-08-30 20:36:10",
+        "from_date": "2008-01-11",
+        "to_date": "2009-07-28"
+        },
+        {
+        "code": "BATS_AAOI",
+        "name": "Short Sale Market for Stock Symbol AAOI on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAOI<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2013-09-26",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAON",
+        "name": "Short Sale Market for Stock Symbol AAON on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAON<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2008-01-02",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAP",
+        "name": "Short Sale Market for Stock Symbol AAP on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AAP<br><br>Exchange: BATS",
+        "refreshed_at": "2021-09-28 19:00:53",
+        "from_date": "2008-01-02",
+        "to_date": "2021-09-27"
+        },
+        {
+        "code": "BATS_AAPB",
+        "name": "Short Sale Market for Stock Symbol AApB on BATS Exchange",
+        "description": "Aggregate short sale volumes<br><br>Stock Symbol: AApB<br><br>Exchange: BATS",
+        "refreshed_at": "2016-11-07 19:03:00",
+        "from_date": "2014-09-25",
+        "to_date": "2016-10-31"
+        }
+    ]
+}
+
+// pseudo API
+export const getBatsCodes = () =>
+  new Promise((resolve, reject) => {
+    if (!batsCodes) {
+      return setTimeout(
+        () => reject(new Error('BATS codes not found')),
+        250
+      );
+    }
+
+    setTimeout(() => resolve(Object.values(batsCodes)), 250);
+  });
+
